a10da16f170eebd9822ec3d8ed71802807445067
argenisgam <argenis.gamboa@tdea.edu.co>
2026-07-23 17:22:24 -0500

    pg

--- config/db.js
+++ config/db.js
@@ -1,10 +1,12 @@
 const { Pool } = require('pg');
+require('dotenv').config();
+
 const pool = new Pool({
-    host: 'localhost',
-    port: 5432,
-    user: 'postgres',
-    password: '123456',
-    database: 'biblioteca'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE
 });
 pool.query('SELECT NOW()', (error, resultado)=>{
     if(error){
@@ -13,4 +15,3 @@ console.log(error);
     console.log(resultado.rows);
 }
 });
-module.exports=pool;
